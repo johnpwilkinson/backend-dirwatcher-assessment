93288d045d4532b12061ffeea521026f935b9f32
John Wilkinson <johnpwilkinson@outlook.com>
2020-07-16 00:00:58 -0400

    refactored the code, added custom terminal aesthetics

--- dirwatcher.py
+++ dirwatcher.py
@@ -1,109 +1,137 @@
-import signal
-import time
+import sys
 import argparse
 import os
 import logging
+import time
+import signal
+from blessings import Terminal
 
+logging.basicConfig(level=logging.INFO,
+                    format='%(levelname)s:%(message)s:')
+logger = logging.getLogger(__name__)
 
+dir_dict = {}
+init_time = time.time()
 exit_flag = False
 
+t = Terminal()
+w = t.width
+g = t.green
 
 
-dir_files = {}
-
-def signal_handler(sig_num, frame):
+def get_directory_structure(argz):
     """
-    This is a handler for SIGTERM and SIGINT. Other signals can be mapped here as well (SIGHUP?)
-    Basically, it just sets a global flag, and main() will exit its loop if the signal is trapped.
-    :param sig_num: The integer signal number that was trapped from the OS.
-    :param frame: Not used
-    :return None
+    Creates a dictionary that represents the
+    contents of the target dir
     """
-    # log the associated signal name
-    logger.warn('Received ' + signal.Signals(sig_num).name)
-    exit_flag = True
+    flash_dict = {}
+    try:
+        if os.path.isdir(argz.dirname[0]):
+            for dir_content in os.walk(argz.dirname[0]):
+                files = dir_content[2]
+            for file in files:
+                if file.endswith(argz.extname[0]):
+                    flash_dict.setdefault(file, [])
+        else:
+            logger.info(f"{argz.dirname[0]} does not exist ")
+    except Exception as e:
+        logger.exception(f"Alert! {e}")
+    compare_dict_sizes(flash_dict, argz)
 
-def get_directory_structure(rootdir):
+
+def compare_dict_sizes(flash_dict, argz):
     """
-    Creates a nested dictionary that represents the folder structure of rootdir
+    Compares flash_dict to dir_dict to determine if the size of the
+    dict has changed... and if so, in what way.
     """
-    dir = {}
-    rootdir = rootdir.rstrip(os.sep)
-    for path, dirs, files in os.walk(rootdir):
-       for file in files:
-           if file not in dir.keys():
-               dir[file] = 1
-    print(dir)
-    return dir
+    try:
+        for flash_file in flash_dict:
+            if flash_file not in dir_dict:
+                logger.info(
+                    f"{g}{flash_file} has been added to {argz.dirname[0]}")
+                dir_dict[flash_file] = []
+        for actualfile in list(dir_dict):
+            if actualfile not in flash_dict:
+                logger.info(
+                    f"{g}{actualfile} has been removed from {argz.dirname[0]}")
+                del dir_dict[actualfile]
+    except Exception as e:
+        logger.exception(f"Alert! {e}")
+    scan_for_magic_text(argz)
 
-def scan_single_file(file_to_scan, magic_text):
-    """function to scan a single file for the 'magic string'"""
-    with open(file_to_scan) as f:
-        datafile = f.readlines()
-        for line_index, line in enumerate(datafile):
-            if magic_text in line:
-                # print("true")
-                # if file is not in the 
-                if file_to_scan not in dir_files.keys():
-                    dir_files[file_to_scan] = line_index
-                if file_to_scan in dir_files.keys() and line_index >= dir_files[file_to_scan]
-                    dir_files[file_to_scan] +=1
 
-detect_added_files():
-    pass
-detect_removed_files():
-    pass
-watch_directory():
-    pass
-def magic_text_locator(dir, extension, magic_text):
+def scan_for_magic_text(argz):
+    """
+    Scans the files for instances of the magic text
+    """
+    try:
+        for file in dir_dict:
+            with open(argz.dirname[0]+"/"+file, "r") as f:
+                lines = f.readlines()
+                for i, line in enumerate(lines):
+                    if argz.magic_text[0] in line:
+                        if i not in dir_dict[file]:
+                            dir_dict[file].append(i)
+                            logger.info(
+                                f"{g}magic text in {file} @ line {str(i+1)}")
+    except Exception as e:
+        logger.exception(f"Alert! {e}")
 
-    pass
 
-def dir_watcher_parser(args):
-    """This is the functions that creates the argument parser. In the past this 
-        has been donein main(), but main() seems a little crowded at the moment"""
+def create_parser():
+    """Command Line Parser"""
     parser = argparse.ArgumentParser(
-                                    prog='dir_watcher',
-                                    description='Watches Dirs for magic strings',
-                                    usage='%(prog)s [options]',
-                                    epilog="Lookin for those magic strings")
-
-    parser.add_argument('dirname', help='What Dir to watch')
-    parser.add_argument('magictext', help='what is the magic text')
-    parser.add_argument('--extname', help='What type of file extension to search with-in', default='.txt')
-    parser.add_argument('--howoften', help='(Number) how often to search for magic-text', default=1)
+        prog='dir_watcher',
+        description='Watches Dirs for magic strings',
+        usage='%(prog)s [options]',
+        epilog="Lookin for those magic strings")
+    parser.add_argument('dirname', help='What Dir to watch?', nargs='+')
+    parser.add_argument(
+        'magic_text', help='What is the magic text?', nargs='+')
+    parser.add_argument('--extname', default='.txt',
+                        help='What type of file?', nargs='+')
+    parser.add_argument('--how_often', default=1,
+                        help='How long between scans?',
+                        nargs='+')
     return parser
 
 
-def main():
-    # maybe put in while / try
-    starting_time = time.time()
-    parser = dir_watcher_parser()
-    args = parser.parse_args()
-    # Hook into these two signals from the OS
+def signal_handler(sig_num, frame):
+    """
+     This is a handler for SIGTERM and SIGINT. Other signals can be mapped
+    here as well. Basically, it just sets a global flag, and main() will
+    exit its loop if the signal is trapped.
+    """
+    global exit_flag
+    run_time = time.time() - init_time
+    message = "Dir_Watcher Terminating"
+    r = f"Run Time was {run_time}"
+    b = t.bold_red
+    logger.info(
+        f"{b}\n{'$'*w}\n{message.center(int(w))}\n{r.center(int(w))}\n{'$'*w}")
+    exit_flag = True
+
+
+def main(args):
+    """executes Dir watcher, logs errors, and listens for OS Signals"""
+    message = "Dir_Watcher Initialized"
+    logger.info(
+        f"{t.bold_green}\n{'$'*w}\n{message.center(int(w))}\n{'$'*w}\n")
+
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
-    # Now my signal_handler will get called if OS sends
-    # either of these to my process.
+    parser = create_parser()
+    argz = parser.parse_args(args)
+    polling_interval = argz.how_often
 
     while not exit_flag:
         try:
-            magic_text_locator(args.dirname, args.magictext, args.extname)
+            get_directory_structure(argz)
         except Exception as e:
-            # This is an UNHANDLED exception
-            # Log an ERROR level message here
-            pass
-
-        # put a sleep inside my while loop so I don't peg the cpu usage at 100%
-        time.sleep(args.howoften)
-
-    # final exit point happens here
-    # Log a message that we are shutting down
-    # Include the overall uptime since program start
+            logger.exception(f"Alert! {e}")
+            get_directory_structure([])
+        time.sleep(polling_interval)
 
 
-
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main(sys.argv[1:])
